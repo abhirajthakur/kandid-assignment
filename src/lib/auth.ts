7b365679602b4917593ec55d92d01de5eab16346
abhirajthakur <abhirajthakur124@gmail.com>
2025-09-09 01:49:25 +0530

    feat: implement Better Auth with email/password and Google OAuth

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  plugins: [nextCookies()],
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
