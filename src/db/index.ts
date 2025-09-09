9f0086edc07072ad2092bcdc65f19861b00005a3
Abhiraj Thakur <abhirajthakur124@gmail.com>
2025-09-09 22:34:03 +0530

    add proper campaign-lead relationship in the schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,10 @@
-import "dotenv/config";
+import { env } from "@/lib/env";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
